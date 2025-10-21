83720d16db15856352b04f3034c01ecfd79e8dfe
léo jackson <leojackson@MacBook-Air-de-leo.local>
2025-10-21 11:26:51 +0200

    fix data access

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 import kagglehub
 import os
 import csv
+import pandas
 
 # Définir le répertoire de travail actuel (dossier du projet)
 project_dir = os.getcwd()
-print(f"Répertoire de travail: {project_dir}")
 
-# Définir le répertoire de téléchargement relatif au projet
-data_dir = os.path.join(project_dir, "data")
-os.environ['KAGGLE_CACHE_DIR'] = data_dir
-
-# Créer le répertoire s'il n'existe pas
-os.makedirs(data_dir, exist_ok=True)
-print(f"Répertoire des données: {data_dir}")
-
-path = kagglehub.dataset_download("adilimadeddinehosni/multi-resistance-antibiotic-susceptibility")
-
-print("Path to dataset files:", path)
+table_path = os.path.join(project_dir, "antibiotics_multi_resistance.csv")
+print(table_path)
+table = pandas.read_csv(table_path, delimiter=',')
+print(table.head())
