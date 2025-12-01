7cd2a78c6b1d91a73edf42831e3c969c95fa3dd2
Marie-diégo Viagni Crédo Ayité AYIVI <credoay@gmail.com>
2025-12-02 00:30:39 +0100

    Restructuration project

--- Readme.md
+++ Readme.md
@@ -1,3 +1,35 @@
-### Instructions
+## Instructions
 
 
+### Description du projet
+
+lorem ipsumm lorem ipsumm lorem ipsumm lorem ipsumm lorem ipsumm lorem ipsumm lorem ipsumm
+
+### Execution du projet
+
+- Installation des dépendances
+
+> Se positionner sur le répertoire **processing** en s'assurant d'être dans un environnement python puis executer la commande suivante
+
+```shell
+    cd processing && pip install -r ./processing/main.py
+```
+
+> Ensuite executer cette commande pour installer les jeux de données datasets
+```shell
+    py ./main.py
+    py ./processing/create_geojson.py
+```
+
+- Démarrer le projet
+
+> Revenir au répertoire parent et installer les dépendances 
+
+```shell
+    cd .. && npm install
+```
+
+> Lancement du projet
+```shell
+    npm run start
+```
--- processing/main.py
+++ processing/main.py
@@ -1,17 +1,59 @@
+import kagglehub
 import os
 import pandas
 from models import DataPreparation
-import kagglehub
 
-# Définir le répertoire de travail actuel (dossier du projet)
+
 project_dir = os.getcwd()
-sample_table_path = os.path.join(project_dir, "flightData/flight_data_2024_sample.csv")
-table_path = os.path.join(project_dir, "flightData/flight_data_2024_sample.csv")
+
+# Specify your desired folder
+data_folder = "./data"
+
+#folders for each persons
+data_folder_leo = "./data/leo"
+data_folder_credo = "./data/credo"
+data_folder_delphine = "./data/delphine"
+data_folder_khadija = "./data/khadija"
+
+# Create folders if it doesn't exist
+if not os.path.exists(data_folder):
+    os.makedirs(data_folder)
+
+if not os.path.exists(data_folder_leo):
+    os.makedirs(data_folder_leo)
+
+if not os.path.exists(data_folder_credo):
+    os.makedirs(data_folder_credo)
+
+if not os.path.exists(data_folder_delphine):
+    os.makedirs(data_folder_delphine)
+
+if not os.path.exists(data_folder_khadija):
+    os.makedirs(data_folder_khadija)
+
+# Download dataset
+path = kagglehub.dataset_download("hrishitpatil/flight-data-2024")
+
+# Find and copy only CSV files
+for file in os.listdir(path):
+    if file.endswith('.csv'):
+        source = os.path.join(path, file)
+        destination = os.path.join(data_folder, file)
+
+        # Copy file
+        with open(source, 'rb') as src:
+            with open(destination, 'wb') as dst:
+                dst.write(src.read())
+
+        print(f"Downloaded: {file} to {data_folder}")
+
+
+sample_table_path = os.path.join(project_dir, data_folder + "/flight_data_2024_sample.csv")
+table_path = os.path.join(project_dir, data_folder + "/flight_data_2024.csv")
 
 sample_table = pandas.read_csv(sample_table_path, delimiter=',')
 table = pandas.read_csv(table_path, delimiter=',')
-print(type(table))
-print(table.head())
+
 
 # Créer une instance de DataPreparation
 data_prep = DataPreparation()
@@ -25,7 +67,7 @@ cities_states = pandas.concat([
 
 
 # Sauvegarder la liste des villes
-cities_states.to_csv("flightData/cities_states_list.csv", index=False)
+cities_states.to_csv( data_folder + "/cities_states.csv", index=False)
 print(f"Liste des villes sauvegardée : {len(cities_states)} villes uniques")
 
 
@@ -39,20 +81,23 @@ sample_table = data_prep.add_carrier_name(data=sample_table)
 sample_table = data_prep.add_booleanize_delays(data=sample_table)
 sample_table = data_prep.add_week_number(data=sample_table)
 sample_table = data_prep.add_distance_categories(data=sample_table)
+
 # save data after renaming
-sample_path = os.path.join(project_dir, "flightData/flight_data_2024_sample_renamed.csv")
+sample_path = os.path.join(project_dir, data_folder + "/flight_data_2024_sample_renamed.csv")
 sample_table.to_csv(sample_path, index=False)
-path = os.path.join(project_dir, "flightData/flight_data_2024_renamed.csv")
+path = os.path.join(project_dir, data_folder + "/flight_data_2024_renamed.csv")
 table.to_csv(path, index=False)
 
-
 # Afficher le résultat final
-print("\nDonnées finales:")
-print(table.head())
-print(f"\nShape du DataFrame: {table.shape}")
-print(f"Colonnes: {list(table.columns)}")
+# print("\nDonnées finales:")
+# print(table.head())
+# print(f"\nShape du DataFrame: {table.shape}")
+# print(f"Colonnes: {list(table.columns)}")
 
-### Agrégations pour Kadija ###
+
+'''
+Agrégations pour Kadija
+'''
 
 # Agréger les données par semaine et par compagnie aérienne
 agregated_data = data_prep.agregate_data(
@@ -63,7 +108,7 @@ agregated_data = data_prep.agregate_data(
         'cancelled': 'sum',
         'flight_number': 'count'
     },
-    filepath="flightData/dataKadija/aggregated_by_week_and_carrier.csv"
+    filepath=f"{data_folder_khadija}/aggregated_by_week_and_carrier.csv"
 )
 
 # Agréger les données par mois et par compagnie aérienne
@@ -75,7 +120,7 @@ agregated_data = data_prep.agregate_data(
         'cancelled': 'sum',
         'flight_number': 'count'
     },
-    filepath="flightData/dataKadija/aggregated_by_month_and_carrier.csv"
+    filepath=f"{data_folder_khadija}/aggregated_by_month_and_carrier.csv"
 )
 
 # Agréger les données par type de retard et par compagnie aérienne
@@ -92,10 +137,12 @@ agregated_data = data_prep.agregate_data(
         'cancelled': 'sum',
         'flight_number': 'count'
     },
-    filepath="flightData/dataKadija/aggregated_by_late_type_and_carrier.csv"
+    filepath=f"{data_folder_khadija}/aggregated_by_late_type_and_carrier.csv"
 )
 
-### Agrégations pour Delphine ###
+'''
+Agrégations pour Delphine
+'''
 
 # Agréger les données par semaine et par catégorie de distance
 agregated_data = data_prep.agregate_data(
@@ -106,7 +153,7 @@ agregated_data = data_prep.agregate_data(
         'cancelled': 'sum',
         'flight_number': 'count'
     },
-    filepath="flightData/dataDelphine/aggregated_by_week_and_distance_category.csv"
+    filepath=f"{data_folder_delphine}/aggregated_by_week_and_distance_category.csv"
 )
 
 # Agréger les données par mois et par catégorie de distance
@@ -118,7 +165,7 @@ agregated_data = data_prep.agregate_data(
         'cancelled': 'sum',
         'flight_number': 'count'
     },
-    filepath="flightData/dataDelphine/aggregated_by_month_and_distance_category.csv"
+    filepath=f"{data_folder_delphine}/aggregated_by_month_and_distance_category.csv"
 )
 
 # Agréger les données par type de retard et par catégorie de distance
@@ -135,10 +182,13 @@ agregated_data = data_prep.agregate_data(
         'cancelled': 'sum',
         'flight_number': 'count'
     },
-    filepath="flightData/dataDelphine/aggregated_by_late_type_and_distance_category.csv"
+    filepath=f"{data_folder_delphine}/aggregated_by_late_type_and_distance_category.csv"
 )
 
-### Agrégations pour Léo ###
+
+'''
+Agrégations pour Léo
+'''
 
 # Agréger les données par semaine et par ville d'origine
 agregated_data = data_prep.agregate_data(
@@ -154,7 +204,7 @@ agregated_data = data_prep.agregate_data(
         'cancelled': 'sum',
         'flight_number': 'count'
     },
-    filepath="flightData/dataLeo/aggregated_by_week_and_origin_city.csv"
+    filepath=f"{data_folder_leo}/aggregated_by_week_and_origin_city.csv"
 )
 
 # Agréger les données par mois et par ville d'origine
@@ -171,7 +221,7 @@ agregated_data = data_prep.agregate_data(
         'cancelled': 'sum',
         'flight_number': 'count'
     },
-    filepath="flightData/dataLeo/aggregated_by_month_and_origin_city.csv"
+    filepath=f"{data_folder_leo}/aggregated_by_month_and_origin_city.csv"
 )
 
 
@@ -190,7 +240,7 @@ agregated_data = data_prep.agregate_data(
         'cancelled': 'sum',
         'flight_number': 'count'
     },
-    filepath="flightData/dataLeo/aggregated_by_week_and_dest_city.csv"
+    filepath=f"{data_folder_leo}/aggregated_by_week_and_dest_city.csv"
 )
 
 # Agréger les données par mois et par ville d'arrivée
@@ -207,11 +257,14 @@ agregated_data = data_prep.agregate_data(
         'cancelled': 'sum',
         'flight_number': 'count'
     },
-    filepath="flightData/dataLeo/aggregated_by_month_and_dest_city.csv"
+    filepath=f"{data_folder_leo}/aggregated_by_month_and_dest_city.csv"
 )
 
 
-### Agrégations pour Credo ###
+
+'''
+Agrégations pour Credo
+'''
 
 # Agréger les données par semaine et par itinéraire
 agregated_data = data_prep.agregate_data(
@@ -222,7 +275,7 @@ agregated_data = data_prep.agregate_data(
         'cancelled': 'sum',
         'flight_number': 'count'
     },
-    filepath="flightData/dataCredo/aggregated_by_week_and_route.csv"
+    filepath=f"{data_folder_credo}/aggregated_by_week_and_route.csv"
 )
 
 # Agréger les données par mois et par itinéraire
@@ -234,7 +287,7 @@ agregated_data = data_prep.agregate_data(
         'cancelled': 'sum',
         'flight_number': 'count'
     },
-    filepath="flightData/dataCredo/aggregated_by_month_and_route.csv"
+    filepath=f"{data_folder_credo}/aggregated_by_month_and_route.csv"
 )
 
 # Agréger les données par type de retard et par itinéraire
@@ -251,5 +304,6 @@ agregated_data = data_prep.agregate_data(
         'cancelled': 'sum',
         'flight_number': 'count'
     },
-    filepath="flightData/dataCredo/aggregated_by_late_type_and_route.csv"
+    filepath=f"{data_folder_credo}/aggregated_by_late_type_and_route.csv"
 )
+
